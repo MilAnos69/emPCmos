6d23c63e4e7150091149e5d5c9da608a39b7b3e6
caleyr <sergiofon99@gmail.com>
2020-11-17 22:24:05 -0500

    nose

--- app/src/main/java/com/example/empcmos/MainActivity.kt
+++ app/src/main/java/com/example/empcmos/MainActivity.kt
@@ -51,7 +51,8 @@ class MainActivity() : AppCompatActivity(), ComunicarFragmentos {
     lateinit var listUsuarios: ArrayList<EUsuarios>
     private  var rol: String=""
 
-
+    //hi
+    //ListaPartes
     lateinit var listaFiltrada : ArrayList<EProducto>
     //Datos Producto
     var listaPcs = ArrayList<EProducto>()
